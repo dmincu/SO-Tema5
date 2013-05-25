a297ac492dcb9eb16ab01e7775bb261d31fcea3f
Diana Mincu <diana.mincu@cti.pub.ro>
2013-05-25 13:21:34 +0300

    Changed dynamic file like in aio lab

--- src/server.c
+++ src/server.c
@@ -47,23 +47,22 @@ struct connection {
 	int sockfd;
 	int fd;
 	char pathname[BUFSIZ];
+
 	/* buffers used for receiving messages and then echoing them back */
 	char recv_buffer[BUFSIZ];
 	size_t recv_len;
 	char send_buffer[BUFSIZ];
 	size_t send_len;
 	enum connection_state state;
-	struct iocb *piocb;
+
+	struct iocb *iocb;
+	struct iocb **piocb;
 	int iocbs;
 	int efd;
 };
 
-/* AIO context */
-io_context_t ctx;
-/* AIO events (vector result) */
-struct io_event events[NUM_OPS];
+char buffer[BUFSIZ];
 
-char *buffer;
 int eefd;
 
 /*
@@ -195,6 +194,34 @@ remove_connection:
 }
 
 /*
+ * wait for asynchronous I/O operations
+ * (eventfd or io_getevents)
+ */
+static void wait_aio(io_context_t ctx, int nops)
+{
+	struct io_event *events;
+	int rc;
+
+	/* TODO 1 - alloc structure */
+	events = (struct io_event *) malloc(nops * sizeof(struct io_event));
+	if (events == NULL){
+		ERR("malloc");
+	}
+
+	/* Wait for async operations to finish 
+	 *
+	 * 	Use only io_getevents()
+	 */
+	rc = io_getevents(ctx, nops, nops, events, NULL);
+	if (rc < 0){
+		ERR("io_getevents");
+	}
+
+	free(events);
+}
+
+
+/*
  * Send message on socket.
  * Store message in send_buffer in struct connection.
  */
@@ -202,7 +229,7 @@ remove_connection:
 static enum connection_state send_message(struct connection *conn)
 {
 	ssize_t bytes_sent;
-	int rc,i;
+	int rc, i;
 	char abuffer[64];
 
 	rc = get_peer_address(conn->sockfd, abuffer, 64);
@@ -233,7 +260,7 @@ static enum connection_state send_message(struct connection *conn)
 		fstat(conn->fd, buf);
 
 		/* TODO: trimis fisiere si in mod dinamic */
-		if (strstr(request_path, "static") != -1){
+		if (strstr(request_path, "static") != NULL){
 			fprintf(stderr, "Using sendfile\n");
 			off_t total_sent = 0;
 			int size = BUFSIZ;
@@ -241,11 +268,10 @@ static enum connection_state send_message(struct connection *conn)
 			while (total_sent < buf->st_size){
 				if (total_sent + BUFSIZ >= buf->st_size)
 					size = total_sent + BUFSIZ - buf->st_size;
-				fprintf(stderr, "%i %i\n", total_sent, buf->st_size);
+				fprintf(stderr, "%i %i\n", (int)total_sent, (int)buf->st_size);
 				rc = sendfile(conn->sockfd, conn->fd, NULL, buf->st_size);
 				if (rc == -1){
 					ERR("sendfile\n");
-					//goto remove_connection;
 				}
 				else
 					total_sent += rc;
@@ -253,40 +279,52 @@ static enum connection_state send_message(struct connection *conn)
 		}
 		else{
 			/* io_submit etc */
+			int n_aio_ops;
+
 			/* initializez o noua conexiune aici, eventfd?*/
 			conn->efd = eventfd(0, 0);
-        	eefd = conn->efd;
-        	
-			conn->iocbs = buf->st_size / BUFSIZ + 1;
-			conn->piocb = malloc(conn->iocbs * sizeof(struct iocb));
-			buffer = malloc(buf->st_size * sizeof(char));
+			eefd = conn->efd;
+
+			/* AIO context */
+			io_context_t ctx;
+
+			conn->iocbs = buf->st_size / BUFSIZ + (buf->st_size % BUFSIZ == 0 ? 0 : 1);
 			
-			for (i=0; i< conn->iocbs - 1; i++)
-			{
-				memset(&(conn->piocb[i]), 0, sizeof(struct iocb));
-				io_prep_pread(&(conn->piocb[i]), conn->fd, buffer+i*BUFSIZ, BUFSIZ, 0);
-				io_set_eventfd(&(conn->piocb[i]), conn->efd);
+			/* Allocate iocb and piocb */
+			conn->iocb = (struct iocb *) malloc(conn->iocbs * sizeof(*(conn->iocb)));
+			if (conn->iocb == NULL){
+				ERR("malloc");
 			}
-        	
-        	memset(&(conn->piocb[conn->iocbs-1]), 0, sizeof(struct iocb));
-			io_prep_pread(&(conn->piocb[i]), conn->fd, buffer+(conn->iocbs-1)*BUFSIZ, buf->st_size - (conn->iocbs-1)*BUFSIZ, 0);
-			io_set_eventfd(&(conn->piocb[conn->iocbs-1]), conn->efd);
-        	
-        	io_submit(ctx, 1, &(conn->piocb));
-        	
-        	w_epoll_add_ptr_in(epollfd,conn->efd,conn);
-        	
-        	/* verific daca s-a terminat aia dinamica */
-			//u_int64_t efd_val;
-			//read(conn->efd, &efd_val, sizeof(efd_val));
-			/*
-			rc = io_getevents(ctx, 1,
-				               NUM_OPS,
-				               events, 
-				               NULL);  
-			*/
-			
-			return STATE_DATA_SENT; 	
+
+			conn->piocb = (struct iocb **) malloc(conn->iocbs * sizeof(*(conn->piocb)));
+			if (conn->piocb == NULL){
+				ERR("malloc");
+			}
+
+			for (i = 0; i < conn->iocbs; i++)
+			{
+				io_prep_pwrite(&(conn->iocb[i]), conn->fd, buffer, BUFSIZ, 0);
+				conn->piocb[i] = &(conn->iocb[i]);
+				io_set_eventfd(&(conn->iocb[i]), conn->efd);
+			}
+
+			/* Setup aio context */
+			rc = io_setup(conn->iocbs, &ctx);
+			if (rc < 0){
+				ERR("io_setup");
+			}
+
+			/* Submit aio */
+			n_aio_ops = io_submit(ctx, conn->iocbs, conn->piocb);
+			if (n_aio_ops < 0){
+				ERR("io_submit");
+			}
+
+			/* Wait for completion*/
+			wait_aio(ctx, conn->iocbs);
+
+			/* Destroy aio context */
+			io_destroy(ctx);
 		}
 	
 		free(buf);
@@ -365,9 +403,6 @@ int main(int argc, char **argv)
 	DIE(rc < 0, "w_epoll_add_fd_in");
 
 	dlog(LOG_INFO, "Server waiting for connections on port %d\n", AWS_LISTEN_PORT);
-	
-	/* init context for AIO */
-	io_setup(NUM_OPS, &ctx);
 
 	/* server main loop */
 	while (1) {
@@ -382,38 +417,11 @@ int main(int argc, char **argv)
 		 *   - new connection requests (on server socket)
 		 *   - socket communication (on connection sockets)
 		 */
-
 		if (rev.data.fd == listenfd) {
 			dlog(LOG_DEBUG, "New connection\n");
 			if (rev.events & EPOLLIN)
 				handle_new_connection();
 		}
-		/*else if (rev.data.fd == eefd)
-		{
-			fprintf(stderr,"aici\n");
-			struct connection *conn = ((struct connection *)rev.data.ptr);
-			u_int64_t efd_val;
-			
-			for (i=0; i< conn->iocbs-1; i++)
-				read(conn->efd, &efd_val, sizeof(efd_val)); 
-			
-			//while (efd_val < conn->iocbs)
-				//.
-				//read(conn->efd, &efd_val, sizeof(efd_val));
-			
-			fprintf(stderr,"%i\n", efd_val);
-			
-			for (i=0; i< conn->iocbs-1; i++)
-				bytes_sent = send(conn->sockfd, buffer+i*BUFSIZ, BUFSIZ, 0);
-				
-			bytes_sent = send(conn->sockfd, buffer+(conn->iocbs-1)*BUFSIZ, strlen(buffer+(conn->iocbs-1)*BUFSIZ), 0);
-			
-			rc = w_epoll_remove_ptr(epollfd, conn->sockfd, conn);
-			DIE(rc < 0, "w_epoll_remove_ptr");
-
-			/* remove current connection */
-			/*connection_remove(conn);
-		}*/
 		else {
 			if (rev.events & EPOLLIN) {
 				dlog(LOG_DEBUG, "New message\n");
@@ -426,6 +434,5 @@ int main(int argc, char **argv)
 		}
 	}
 
-	io_destroy(ctx);
 	return 0;
 }
